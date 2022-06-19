6baf852bd57a925f10b6f7c5283c8ae74260e981
Adam <adam@haglund.dev>
2022-06-19 19:55:19 +0200

    remove extra terraform provider

--- example/infra/terraform.tf
+++ example/infra/terraform.tf
@@ -6,10 +6,6 @@ terraform {
       source  = "hashicorp/aws"
       version = "4.17.1"
     }
-    archive = {
-      source  = "hashicorp/archive"
-      version = "2.2.0"
-    }
   }
 
   backend "s3" {
