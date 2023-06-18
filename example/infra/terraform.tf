4264e363273cdd20f0a10bf19c4e1b61ea633c74
Adam <adam@haglund.dev>
2023-06-18 03:20:39 +0200

    update terraform

--- example/infra/terraform.tf
+++ example/infra/terraform.tf
@@ -1,16 +1,16 @@
 terraform {
-  required_version = "1.4.6"
+  required_version = "1.5.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "4.67.0"
+      version = "5.4.0"
     }
   }
 
   backend "s3" {
-    bucket         = "bq-terraform-state"
-    key            = "cf-function-example.tfstate"
-    region         = "eu-west-1"
+    bucket = "bq-terraform-state"
+    key    = "cf-function-example.tfstate"
+    region = "eu-west-1"
   }
 }
